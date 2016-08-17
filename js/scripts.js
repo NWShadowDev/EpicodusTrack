d87e2eee0775cb4fb23e33ee7789b3b390ba7f80
Amber Farrington <amberf0712@outlook.com>
2016-08-17 07:43:21 -0700

    Modify branching to include third epicodus track - still in progress

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,24 @@ $("form").submit(function(event){
   var company =parseInt($("#company").val());
   var language = parseInt($("#language").val());
   var enviro = parseInt($("#enviro").val());
+  var phpDrupal = parseInt($("#phpDrupal").val());
   var successRate = design + interAct + company + language + enviro;
-  
+
+  var designCounter;
+  var javaCounter;
+  var phpCounter;
+
+    if (design === 1 && interAct === 3) {
+      designCounter++;
+    } else if (design === 2) {
+      javaCounter++;
+    } else (interAct === 3 && phpDrupal === 4);
 
   if(successRate <= 5) {
-    $("#designTrack").show();
+    $("#designCSSTrack").show();
 
   }else{
-    $("#programmerTrack").show();
+    $("#javaAndroidTrack").show();
   }
 
   event.preventDefault();
