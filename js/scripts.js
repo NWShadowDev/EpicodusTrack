02265d262e57316c6d252fffb6aa95bc8925b668
Amber <amberf0712@outlook.com>
2016-08-17 22:15:25 -0700

    make some progress in fixing value counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
 $("form").submit(function(event){
 
-  var nameDisplay = $("#nameDisplay").val();
+  var nameDisplay = $(".nameDisplay").val();
   var design = parseInt($("#design").val());
   var interAct = parseInt($("#interAct").val());
   var company =parseInt($("#company").val());
   var language = parseInt($("#language").val());
   var enviro = parseInt($("#enviro").val());
-  var phpDrupal = parseInt($("#phpDrupal").val());
   var successRate = design + interAct + company + language + enviro;
 
+  var counter = 0;
   var designCounter;
   var javaCounter;
   var phpCounter;
 
-    if (design === 1 && interAct === 3) {
+
+    if (design === 2 && interAct === 2) {
       designCounter++;
-    } else if (design === 2) {
+    } else if (design === 1 ) {
       javaCounter++;
-    } else (interAct === 3 && phpDrupal === 4);
+    } else (interAct ===1  && design === 1)
+      phpCounter++;
 
   if(successRate <= 5) {
     $("#designCSSTrack").show();
